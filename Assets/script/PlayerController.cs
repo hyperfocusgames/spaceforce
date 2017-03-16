cfa6d309255060e4147687b446dc8c1e62808691
Tanner Grehawick <tgrehawi@gmail.com>
2017-03-15 21:28:36 -0400

    camera movement with mouse and q/e

---
+++ Assets/script/PlayerController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour {
+
+	public float mouselookSensitivity = 5;
+	public float rollSpeed = 25;
+
+	void Awake() {
+	}
+
+	void Update() {
+		float roll = Input.GetAxis("Roll") * rollSpeed * Time.deltaTime;
+		Vector2 mouse = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+		mouse *= mouselookSensitivity;
+		transform.Rotate(-mouse.y, mouse.x, roll);
+	}
+
+}
